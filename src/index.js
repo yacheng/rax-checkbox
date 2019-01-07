c831318a79291fff87881624d894dcca9a2bb15b
亚城 <narutosun2@163.com>
2019-01-07 17:30:23 +0800

    feat: use index.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {Component, createElement, PropTypes} from 'rax';
 import Image from 'rax-image';
 import View from 'rax-view';
 import Touchable from 'rax-touchable';
+import './index.css';
 
 const CHECKED_ICON = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAwUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzLRwScAAAAPdFJOUwDvEI8wz69QQL/fIHCAYDHs4yUAAACoSURBVCjPfdIBDoMgDAXQ31JE1K33v60UZSJ2NjEmvvgpUIAD6aMoMMBR3YqMoBpmDDXbV1B5nApKUB3/kU9ZR1QLDcKklACPJmvCpWSSPWLbJK0e1bgNDrW4J/3iLmLbSov7oqNyjtFsM5nQUzyOc61xfKPlOOpsr4QbSb0x6uKuNqTdZovrmm+W8KDTJjhUbeGB5LS8SDcChV4GwB0bqWPzMmz/R3QHJwAPwC8jHWQAAAAASUVORK5CYII=';
 const UNCHECKED_ICON = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAYUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzM2vW5DoAAAAHdFJOUwCPEO9AzzBOX/xUAAAASklEQVQoz+2SMRKAQAwCIcmF///YaKl4tYU7Q8O2CwRTD5IBxJJlBSixcKPOFzkzUAmpnGppFCy/+qpqZ2rUJgCbTV/ZbGJ7T/QAwyIE71akwQMAAAAASUVORK5CYII=';
@@ -52,27 +53,16 @@ class CheckBox extends Component {
     }
 
     return (
-      <Touchable role="checkbox" aria-checked={this.state.checked} onPress={this.onChange} style={styles.flexContainer}>
-        <View style={[styles.container, containerStyle]}>
+      <Touchable role="checkbox" aria-checked={this.state.checked} onPress={this.onChange} className="flexContainer">
+        <View className="container" style={containerStyle}>
           <Image
-            style={[styles.checkbox, checkboxStyle]}
+            className="checkbox"
+            style={checkboxStyle}
             source={imageSource} />
         </View>
       </Touchable>
     );
   }
 }
-
-var styles = {
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  checkbox: {
-    width: 40,
-    height: 40
-  }
-};
 
 export default CheckBox;
