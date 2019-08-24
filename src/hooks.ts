c06432315db920fb23ad3038bd00ae5d633fdfae
recover758126 <761647716@qq.com>
2019-08-24 13:39:09 +0800

    feat: update component

---
+++ src/hooks.ts
@@ -0,0 +1,14 @@
+
+import { useEffect, useRef } from 'rax';
+
+export const useUpdateEffect: typeof useEffect = (effect, deps) => {
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      return effect();
+    }
+  }, deps);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,63 +2,88 @@ import {
   createElement,
   CSSProperties,
   useState,
-  useEffect,
+  forwardRef,
   useRef,
   useImperativeHandle,
-  forwardRef,
-  RefForwardingComponent
+  Ref,
 } from 'rax';
 import View from 'rax-view';
 import Image from 'rax-image';
+import { useUpdateEffect } from './hooks';
 
-const CHECKED_ICON = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAwUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzLRwScAAAAPdFJOUwDvEI8wz69QQL/fIHCAYDHs4yUAAACoSURBVCjPfdIBDoMgDAXQ31JE1K33v60UZSJ2NjEmvvgpUIAD6aMoMMBR3YqMoBpmDDXbV1B5nApKUB3/kU9ZR1QLDcKklACPJmvCpWSSPWLbJK0e1bgNDrW4J/3iLmLbSov7oqNyjtFsM5nQUzyOc61xfKPlOOpsr4QbSb0x6uKuNqTdZovrmm+W8KDTJjhUbeGB5LS8SDcChV4GwB0bqWPzMmz/R3QHJwAPwC8jHWQAAAAASUVORK5CYII=';
-const UNCHECKED_ICON = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAYUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzM2vW5DoAAAAHdFJOUwCPEO9AzzBOX/xUAAAASklEQVQoz+2SMRKAQAwCIcmF///YaKl4tYU7Q8O2CwRTD5IBxJJlBSixcKPOFzkzUAmpnGppFCy/+qpqZ2rUJgCbTV/ZbGJ7T/QAwyIE71akwQMAAAAASUVORK5CYII=';
+const CHECKED_ICON =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAwUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzLRwScAAAAPdFJOUwDvEI8wz69QQL/fIHCAYDHs4yUAAACoSURBVCjPfdIBDoMgDAXQ31JE1K33v60UZSJ2NjEmvvgpUIAD6aMoMMBR3YqMoBpmDDXbV1B5nApKUB3/kU9ZR1QLDcKklACPJmvCpWSSPWLbJK0e1bgNDrW4J/3iLmLbSov7oqNyjtFsM5nQUzyOc61xfKPlOOpsr4QbSb0x6uKuNqTdZovrmm+W8KDTJjhUbeGB5LS8SDcChV4GwB0bqWPzMmz/R3QHJwAPwC8jHWQAAAAASUVORK5CYII=';
+const UNCHECKED_ICON =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAYUExURUxpcTMzMzMzMzMzMzMzMzMzMzMzMzMzM2vW5DoAAAAHdFJOUwCPEO9AzzBOX/xUAAAASklEQVQoz+2SMRKAQAwCIcmF///YaKl4tYU7Q8O2CwRTD5IBxJJlBSixcKPOFzkzUAmpnGppFCy/+qpqZ2rUJgCbTV/ZbGJ7T/QAwyIE71akwQMAAAAASUVORK5CYII=';
 
-interface CheckBoxImageSource {
+export interface CheckboxImageSource {
   uri?: string;
   width?: number;
   height?: number;
 }
 
-export interface CheckBoxProps {
+export interface CheckboxProps {
   defaultChecked?: boolean;
+  checked?: boolean;
   containerStyle?: CSSProperties;
   checkboxStyle?: CSSProperties;
-  checkedImage?: CheckBoxImageSource;
-  uncheckedImage?: CheckBoxImageSource;
+  checkedImage?: CheckboxImageSource;
+  uncheckedImage?: CheckboxImageSource;
   onChange?: (checked: boolean) => void;
 }
 
-let firstRendered = true;
-
-const CheckBox: RefForwardingComponent<{}, CheckBoxProps> = forwardRef((props, ref) => {
-  const { defaultChecked = false, containerStyle = {}, checkboxStyle = {}, checkedImage = { uri: CHECKED_ICON }, uncheckedImage = { uri: UNCHECKED_ICON }, onChange } = props;
-  const [checked, setChecked] = useState(defaultChecked);
-  const checkboxRef = useRef(null);
-  const imageSource = checked ? checkedImage : uncheckedImage;
-  useEffect(() => {
-    if (firstRendered) {
-      firstRendered = false;
-    } else {
-      if (onChange) {
-        onChange(checked);
-      }
+const Checkbox = forwardRef(
+  (props: CheckboxProps, ref: Ref<HTMLDivElement>) => {
+    const {
+      defaultChecked = false,
+      checked,
+      containerStyle = {},
+      checkboxStyle = {},
+      checkedImage = { uri: CHECKED_ICON },
+      uncheckedImage = { uri: UNCHECKED_ICON },
+      onChange = () => {}
+    } = props;
+    const [hasChecked, setHasChecked] = useState(defaultChecked);
+    const checkboxRef = useRef<HTMLDivElement>(null);
+    const isChecked = () => {
+      return checked === undefined ? hasChecked : checked;
     }
-  });
-  useImperativeHandle(ref, () => ({
-    _nativeNode: checkboxRef.current._nativeNode
-  }));
-  return (
-    <View ref={checkboxRef} role="checkbox" aria-checked={checked} className="flexContainer" onClick={() => {
-      setChecked(!checked);
-    }}>
-      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10, ...containerStyle }}>
-        <Image
-          style={{ width: 40, height: 40, ...checkboxStyle }}
-          source={imageSource} />
-      </View>
-    </View>
-  );
-});
+    const imageSource = (isChecked()) ? checkedImage : uncheckedImage;
+    useImperativeHandle(ref, () => checkboxRef.current);
+    useUpdateEffect(() => {
+      onChange(isChecked());
+    }, [ hasChecked, checked ])
 
-export default CheckBox;
+    return (
+      <View
+        ref={checkboxRef}
+        role="checkbox"
+        aria-checked={isChecked()}
+        className="flexContainer"
+        onClick={() => {
+          if (checked === undefined) {
+            setHasChecked(!hasChecked);
+          }
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 10,
+            ...containerStyle
+          }}
+        >
+          <Image
+            style={{ width: 40, height: 40, ...checkboxStyle }}
+            source={imageSource}
+          />
+        </View>
+      </View>
+    );
+  }
+);
+
+export default Checkbox;
+
+
