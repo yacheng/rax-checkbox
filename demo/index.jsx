04d3351f7bb20708342f3389c6858f91b9460825
recover758126 <761647716@qq.com>
2019-08-26 21:17:18 +0800

    refactor: update demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,19 +1,46 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { createElement, render, useRef, useState } from 'rax';
+import { createElement, render, useState } from 'rax';
 import DU from 'driver-universal';
 import Checkbox from '../src/index';
 import View from 'rax-view';
+import Text from 'rax-text';
 
 const App = () => {
   const [checked, setChecked] = useState(false);
-  return <View>
-    <Checkbox checked={checked} onChange={(hasChecked) => {
-      alert(`onChange => ${hasChecked}`);
-    }}/>
-    <View style={{ width: 200, height: 200, backgroundColor: 'red' }} onClick={() => {
-      setChecked(!checked);
-    }}/>
-  </View>;
+  return (
+    <View>
+      <Text>Controlled:</Text>
+      <Checkbox
+        checked={checked}
+        onChange={hasChecked => {
+          alert(`Controlled onChange => ${hasChecked}`);
+        }}
+      />
+      <View
+        style={{
+          paddingLeft: 15,
+          paddingRight: 15,
+          backgroundColor: '#1890FF',
+          width: 200,
+          borderRadius: 15,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+        onClick={() => {
+          setChecked(!checked);
+        }}
+      >
+        <Text style={{ color: '#fff' }}>Click Me!</Text>
+      </View>
+      <Text>Uncontrolled:</Text>
+      <Checkbox
+        defaultChecked
+        onChange={hasChecked => {
+          alert(`Uncontrolled onChange => ${hasChecked}`);
+        }}
+      />
+    </View>
+  );
 };
 
 render(<App />, document.body, {
